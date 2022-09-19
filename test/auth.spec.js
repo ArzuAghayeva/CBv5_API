96e6cb278fda93d38f83633b80c63c024fbe607b
dumpdusty <dumpdusty@gmail.com>
2022-09-19 01:22:23 -0700

    add invalid auth tests

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -2,18 +2,53 @@ const axios = require('axios')
 const {expect} = require('chai')
 
 describe('Authentication', function () {
-    it('Successful Login status code',    async function () {
-        const response = await axios.post('https://clientbase-server.herokuapp.com/v5/user/login', {
-            email: 'jacksparrow@pirate.com',
-            password: 'Pirate666!'
+    describe('Login with valid credentials', function () {
+        it('Successful Login status code', async function () {
+            const response = await axios.post('https://clientbase-server.herokuapp.com/v5/user/login', {
+                email: 'jacksparrow@pirate.com',
+                password: 'Pirate666!'
+            })
+                .then(function(response){
+                    return response})
+                .catch(function(error){
+                    return error})
+            expect(response.status).to.eq(200)
         })
-        expect(response.status).to.eq(200)
-     })
-    it('Successful login message',  async function () {
-        const response = await axios.post('https://clientbase-server.herokuapp.com/v5/user/login', {
-            email: 'jacksparrow@pirate.com',
-            password: 'Pirate666!'
+        it('Successful login message', async function () {
+            const response = await axios.post('https://clientbase-server.herokuapp.com/v5/user/login', {
+                email: 'jacksparrow@pirate.com',
+                password: 'Pirate666!'
+            })
+                .then(function(response){
+                    return response})
+                .catch(function(error){
+                    return error})
+            expect(response.data.message).to.eq('Auth success')
         })
-        expect(response.data.message).to.eq('Auth success')
+    })
+
+    describe('Login with invalid credentials', function () {
+        it('Unsuccessful Login status code', async function () {
+            const error = await axios.post('https://clientbase-server.herokuapp.com/v5/user/login', {
+                email: 'test@test.com',
+                password: 'Test123'
+            })
+                    .then(function(response){
+                        return response})
+                    .catch(function(error){
+                        return error})
+            expect(error.response.status).to.eq(400)
+        })
+        it('Unsuccessful login message', async function () {
+            const error = await axios.post('https://clientbase-server.herokuapp.com/v5/user/login', {
+                email: 'test@test.com',
+                password: 'Test123'
+            })
+                .then(function(response){
+                    return response})
+                .catch(function(error){
+                    return error})
+            expect(error.response.data.message).to.eq('Auth failed')
+        })
     })
 })
